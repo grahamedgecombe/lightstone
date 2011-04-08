cc5764eee29675ed2a6c36e1fed60e501779f9c9
Graham Edgecombe <grahamedgecombe@gmail.com>
2011-04-08 17:05:31 +0100

    Added whitespace in Zhuowei Zhang's patch to match the conventions, changed the state/type values to constants and added some TODO comments containing future considerations.

---
+++ src/net/lightstone/model/Blocks.java
@@ -0,0 +1,12 @@
+package net.lightstone.model;
+
+public final class Blocks {
+
+	public static final int TYPE_AIR = 0;
+
+	private Blocks() {
+
+	}
+
+}
+
--- src/net/lightstone/msg/handler/DiggingMessageHandler.java
+++ src/net/lightstone/msg/handler/DiggingMessageHandler.java
@@ -1,10 +1,12 @@
 package net.lightstone.msg.handler;
 
+import net.lightstone.model.Blocks;
 import net.lightstone.model.Player;
 import net.lightstone.msg.DiggingMessage;
 import net.lightstone.model.Chunk;
 import net.lightstone.msg.BlockChangeMessage;
 import net.lightstone.net.Session;
+import net.lightstone.world.World;
 
 /**
  * A {@link MessageHandler} which processes digging messages.
@@ -14,17 +16,29 @@ public final class DiggingMessageHandler extends MessageHandler<DiggingMessage> 
 
 	@Override
 	public void handle(Session session, Player player, DiggingMessage message) {
-		if(message.getState()==2){ //done digging
-			int x=message.getX();
-			int z=message.getZ();
-			int chunkX=x/Chunk.WIDTH+((x<0&&x%16!=0)?-1:0);
-			int chunkZ=z/Chunk.HEIGHT+((z<0&&z%16!=0)?-1:0);
-			Chunk chunk=session.getServer().getWorld().getChunks().getChunk(chunkX, chunkZ);
-			int localX=(x-(chunk.getX()*Chunk.WIDTH))%Chunk.WIDTH;
-			int localZ=(z-(chunk.getZ()*Chunk.HEIGHT))%Chunk.HEIGHT;
-			chunk.setType(localX, localZ, message.getY(), 0); //set to air
-			BlockChangeMessage bcmsg=new BlockChangeMessage(x, message.getY(), z, 0, 0);
-			for(Player p: player.getWorld().getPlayers()){
+		if (player == null)
+			return;
+
+		if (message.getState() == DiggingMessage.STATE_DONE_DIGGING) {
+			World world = player.getWorld();
+
+			int x = message.getX();
+			int z = message.getZ();
+			int y = message.getY();
+
+			// TODO it might be nice to move these calculations somewhere else since they will need to be reused
+			int chunkX = x / Chunk.WIDTH + ((x < 0 && x % Chunk.WIDTH != 0) ? -1 : 0);
+			int chunkZ = z / Chunk.HEIGHT + ((z < 0 && z % Chunk.HEIGHT != 0) ? -1 : 0);
+
+			int localX = (x - chunkX * Chunk.WIDTH) % Chunk.WIDTH;
+			int localZ = (z - chunkZ * Chunk.HEIGHT) % Chunk.HEIGHT;
+
+			Chunk chunk = world.getChunks().getChunk(chunkX, chunkZ);
+			chunk.setType(localX, localZ, y, Blocks.TYPE_AIR);
+
+			// TODO this should also be somewhere else as well... perhaps in the chunk.setType() method itself?
+			BlockChangeMessage bcmsg = new BlockChangeMessage(x, y, z, 0, 0);
+			for (Player p: world.getPlayers()) {
 				p.getSession().send(bcmsg);
 			}
 		}
