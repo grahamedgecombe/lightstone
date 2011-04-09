d8e761e71cd5258b9105d6286efdd488f2e2d1bc
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2011-04-09 14:37:42 -0700

    added chat command to change the time of day

--- src/net/lightstone/msg/handler/ChatMessageHandler.java
+++ src/net/lightstone/msg/handler/ChatMessageHandler.java
@@ -21,8 +21,28 @@ public final class ChatMessageHandler extends MessageHandler<ChatMessage> {
 			session.disconnect("Chat message too long.");
 		} else if (text.startsWith("/")) {
 			// TODO process command
+			processCommand(session, player, text);
 		} else {
 			player.getWorld().broadcastMessage("<" + player.getName() + "> " + text);
+		}
+	}
+
+	private void processCommand(Session session, Player player, String message){
+		String[] command = message.substring(1).split(" ");
+		String action = command[0];
+		if(action.equals("settime")){
+			try{
+				player.getWorld().setTimeOfDay(Long.parseLong(command[1]));
+			}
+			catch(NumberFormatException numex){
+				session.send(new ChatMessage("The time you have entered is not a valid number."));
+			}
+		}
+		else if(action.equals("help")){
+			session.send(new ChatMessage("Commands: settime"));
+		}
+		else{
+			session.send(new ChatMessage("Unknown command."));
 		}
 	}
 
