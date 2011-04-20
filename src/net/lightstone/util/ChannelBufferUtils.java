4c571d7052ebbcdf8d81eee4158c2621990cf591
Graham Edgecombe <grahamedgecombe@gmail.com>
2011-04-20 08:36:09 +0100

    Fixed packet 0x64 which still uses the old UTF-8 encoded strings.

--- src/net/lightstone/util/ChannelBufferUtils.java
+++ src/net/lightstone/util/ChannelBufferUtils.java
@@ -1,5 +1,6 @@
 package net.lightstone.util;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +14,11 @@ import org.jboss.netty.buffer.ChannelBuffer;
  * @author Graham Edgecombe
  */
 public final class ChannelBufferUtils {
+
+	/**
+	 * The UTF-8 character set.
+	 */
+	private static final Charset CHARSET_UTF8 = Charset.forName("UTF-8");
 
 	/**
 	 * Writes a list of parameters (e.g. mob metadata) to the buffer.
@@ -130,6 +136,23 @@ public final class ChannelBufferUtils {
 	}
 
 	/**
+	 * Writes a UTF-8 string to the buffer.
+	 * @param buf The buffer.
+	 * @param str The string.
+	 * @throws IllegalArgumentException if the string is too long
+	 * <em>after</em> it is encoded.
+	 */
+	public static void writeUtf8String(ChannelBuffer buf, String str) {
+		byte[] bytes = str.getBytes(CHARSET_UTF8);
+		if (bytes.length >= 65536) {
+			throw new IllegalArgumentException("Encoded UTF-8 string too long.");
+		}
+
+		buf.writeShort(bytes.length);
+    	buf.writeBytes(bytes);
+	}
+
+	/**
 	 * Reads a string from the buffer.
 	 * @param buf The buffer.
 	 * @return The string.
@@ -143,6 +166,21 @@ public final class ChannelBufferUtils {
 		}
 
 		return new String(characters);
+	}
+
+
+	/**
+	 * Reads a UTF-8 encoded string from the buffer.
+	 * @param buf The buffer.
+	 * @return The string.
+	 */
+	public static String readUtf8String(ChannelBuffer buf) {
+		int len = buf.readUnsignedShort();
+
+		byte[] bytes = new byte[len];
+		buf.readBytes(bytes);
+
+		return new String(bytes, CHARSET_UTF8);
 	}
 
 	/**
