33f86a93e0c94c843ed67f51a9437a9d97d6afd9
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2011-05-13 19:57:04 -0700

    Added more mob ids

--- src/net/lightstone/model/mob/MobLookup.java
+++ src/net/lightstone/model/mob/MobLookup.java
@@ -4,12 +4,34 @@ import java.util.*;
 public class MobLookup{
 	public static final int TYPE_CREEPER = 50;
 	public static final int TYPE_SKELETON = 51;
+	public static final int TYPE_SPIDER = 52;
+	public static final int TYPE_GIANT = 53;
+	public static final int TYPE_ZOMBIE = 54;
+	public static final int TYPE_SLIME = 55;
+	public static final int TYPE_GHAST = 56;
+	public static final int TYPE_ZOMBIE_PIGMAN = 57;
 	public static final int TYPE_PIG = 90;
+	public static final int TYPE_SHEEP = 91;
+	public static final int TYPE_COW = 92;
+	public static final int TYPE_CHICKEN = 93;
+	public static final int TYPE_SQUID = 94;
+	public static final int TYPE_WOLF = 95;
 	private static Map<Integer, String> mobMap = new HashMap<Integer, String>();
 	static {
 		mobMap.put(new Integer(TYPE_CREEPER), "Creeper");
 		mobMap.put(new Integer(TYPE_SKELETON), "Skeleton");
+		mobMap.put(new Integer(TYPE_SPIDER), "Spider");
+		mobMap.put(new Integer(TYPE_GIANT), "Giant");
+		mobMap.put(new Integer(TYPE_ZOMBIE), "Zombie");
+		mobMap.put(new Integer(TYPE_SLIME), "Slime");
+		mobMap.put(new Integer(TYPE_GHAST), "Ghast");
+		mobMap.put(new Integer(TYPE_ZOMBIE_PIGMAN), "PigZombie");
 		mobMap.put(new Integer(TYPE_PIG), "Pig");
+		mobMap.put(new Integer(TYPE_SHEEP), "Sheep");
+		mobMap.put(new Integer(TYPE_COW), "Cow");
+		mobMap.put(new Integer(TYPE_CHICKEN), "Chicken");
+		mobMap.put(new Integer(TYPE_WOLF), "Wolf");
+
 	}
 
 	public static String idToName(int id){
