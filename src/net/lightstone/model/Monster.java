d55e2f81a3242faf475942aec04da1ec5e531faf
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2011-05-14 17:18:25 -0700

    Moved metadata to Mob base class; fixed crouching

--- src/net/lightstone/model/Monster.java
+++ src/net/lightstone/model/Monster.java
@@ -19,10 +19,7 @@ public final class Monster extends Mob {
 	 */
 	private final int type;
 
-	/**
-	 * The monster's metadata.
-	 */
-	private final List<Parameter<?>> metadata = new ArrayList<Parameter<?>>();
+
 
 	/**
 	 * Creates a new monster.
